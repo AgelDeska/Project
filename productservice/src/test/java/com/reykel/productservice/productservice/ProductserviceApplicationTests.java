2ef7b34a7d1a40de9b66c289b8f900671c3defad
Agel Deska Wisamulya <ageldeskawisamulya.com>
2025-11-11 08:35:23 +0700

    commit

---
+++ productservice/src/test/java/com/reykel/productservice/productservice/ProductserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reykel.productservice.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
